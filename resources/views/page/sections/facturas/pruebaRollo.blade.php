5bf4038212cf674cc353a4dc99b9a0ecd1662daf
Rodrigo312-dev <rodrigomachacajimenez312@gmail.com>
2022-06-10 12:01:26 -0400

    factura-rollo

--- resources/views/page/sections/facturas/pruebaRollo.blade.php
+++ resources/views/page/sections/facturas/pruebaRollo.blade.php
@@ -1,143 +1,140 @@
+<div style="width: 8cm; height: auto; padding: 19px">
+
+    <table class="minimalistBlack">
+        <thead>
+            <tr>
+                <th>
+                    <p style="margin-bottom: 0px;">FACTURA</p>
+                    <p style="margin-top: 0px;">CON DERECHO A CREDITO FISCAL</p>
+                    <p>PERTEC S.R.L.</p>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr style="height: 10px"></tr>
+            <tr>
+                <td>Casa Matriz</td>
+            </tr>
+            <tr>
+                <td>Gral. Acha N 330</td>
+            </tr>
+            <tr>
+                <td>Cochabamba-Bolivia</td>
+            </tr>
+            <tr>
+                <td>Telfs.: 4259660 Fax: 4111282</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>Sucursal Nro.: 12</td>
+            </tr>
+            <tr>
+                <td>Punto de Vta.: 8</td>
+            </tr>
+            <tr>
+                <td>AV. VILLAZON KM 5.5</td>
+            </tr>
+            <tr>
+                <td>Tel&eacute;fono: 4250800</td>
+            </tr>
+            <tr style="height: 15px"></tr>
+        </tbody>
+    </table>
+    <hr>
 
 
-<table class="minimalistBlack">
-    <thead>
-        <tr>
-            <th>
-                <p style="margin-bottom: 0px;">FACTURA</p>
-                <p style="margin-top: 0px;">CON DERECHO A CREDITO FISCAL</p>
-                <p>PERTEC S.R.L.</p>
-            </th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr style="height: 10px"></tr>
-        <tr>
-            <td>Casa Matriz</td>
-        </tr>
-        <tr>
-            <td>Gral. Acha N 330</td>
-        </tr>
-        <tr>
-            <td>Cochabamba-Bolivia</td>
-        </tr>
-        <tr>
-            <td>Telfs.: 4259660 Fax: 4111282</td>
-        </tr>
-        <tr>
-            <td>&nbsp;</td>
-        </tr>
-        <tr>
-            <td>Sucursal Nro.: 12</td>
-        </tr>
-        <tr>
-            <td>Punto de Vta.: 8</td>
-        </tr>
-        <tr>
-            <td>AV. VILLAZON KM 5.5</td>
-        </tr>
-        <tr>
-            <td>Tel&eacute;fono: 4250800</td>
-        </tr>
-        <tr style="height: 15px"></tr>
-    </tbody>
-</table>
+    <table class="section1">
+        <tbody>
+            <tr>
+                <td colspan="2">NIT
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</td>
+                <td colspan="2">1023109029</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td colspan="2">FACTURA No. &nbsp;:</td>
+                <td colspan="2">0000000001</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td colspan="3">COD. AUTORIZACION: </td>
+                <td colspan="3">4601038E819DF3D13B41E49</td>
+            </tr>
+            <tr>
+                <td colspan="6">11B270A60859F5E06F8571B24ACB886D74</td>
+            </tr>
+        </tbody>
+    </table>
+    <hr>
+    <table class="section1">
+        <tbody>
+            <tr>
+                <td colspan="3">LUGAR Y FECHA EMISION :</td>
+                <td colspan="3">Sacaba 6/6/2022 HRS: 11:56</td>
+            </tr>
+            <tr>
+                <td colspan="3">NOMBRE / RAZON SOCIAL:</td>
+                <td colspan="3">SALINAS</td>
+            </tr>
+            <tr>
+                <td colspan="3">NIT/CI/CEX
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                </td>
+                <td colspan="3">5905524</td>
+            </tr>
+            <tr>
+                <td colspan="3">CODIGO CLIENTE &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</td>
+                <td colspan="3">5905524</td>
+            </tr>
+        </tbody>
+    </table>
+    <hr>
 
+    <table class="section2">
+        <thead>
+            <tr style="height: 3px;"></tr>
+            <tr>
+                <th colspan="2">CODIGO</th>
+                <th>PRODUCTO</th>
+                <th>DESCRIPCION</th>
+                <th>MEDIDA</th>
+            </tr>
+        </thead>
+    </table>
+    <table class="section2">
+        <thead>
+            <tr>
+                <th>UD&nbsp;</th>
+                <th>CANTIDAD&nbsp;</th>
+                <th>PRECIO&nbsp;</th>
+                <th>DESCUENTO&nbsp;&nbsp;</th>
+                <th>SUBTOTAL</th>
+            </tr>
+        </thead>
+    </table>
+    <hr>
 
-
-<table class="section1">
-    <tbody>
-        <tr style="height: 3px; border-top: dashed; border-top-width: 1px;"></tr>
-        <tr>
-            <td colspan="2">NIT
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</td>
-            <td colspan="2">1023109029</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td colspan="2">FACTURA No. &nbsp;:</td>
-            <td colspan="2">0000000001</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td colspan="3">COD. AUTORIZACION: </td>
-            <td colspan="3">4601038E819DF3D13B41E49</td>
-        </tr>
-        <tr>
-            <td colspan="6">11B270A60859F5E06F8571B24ACB886D74</td>
-        </tr>
-        <tr style="height: 6px; border-bottom: dashed; border-bottom-width: 1px;"></tr>
-        <tr style="height: 3px;"></tr>
-        <tr>
-            <td colspan="3">LUGAR Y FECHA EMISION :</td>
-            <td colspan="3">Sacaba 6/6/2022 HRS: 11:56</td>
-        </tr>
-        <tr>
-            <td colspan="3">NOMBRE / RAZON SOCIAL:</td>
-            <td colspan="3">SALINAS</td>
-        </tr>
-        <tr>
-            <td colspan="3">NIT/CI/CEX
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
-            </td>
-            <td colspan="3">5905524</td>
-        </tr>
-        <tr>
-            <td colspan="3">CODIGO CLIENTE &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</td>
-            <td colspan="3">5905524</td>
-        </tr>
-        <tr style="height: 6px; border-bottom: dashed; border-bottom-width: 1px;"></tr>
-    </tbody>
-</table>
-
-
-<table class="section2">
-    <thead>
-        <tr style="height: 3px;"></tr>
-        <tr>
-            <th colspan="2">CODIGO</th>
-            <th>PRODUCTO</th>
-            <th>DESCRIPCION</th>
-            <th>MEDIDA</th>
-        </tr>
-    </thead>
-</table>
-<table class="section2">
-    <thead>
-        <tr>
-            <th>UD&nbsp;</th>
-            <th>CANTIDAD&nbsp;</th>
-            <th>PRECIO&nbsp;</th>
-            <th>DESCUENTO&nbsp;&nbsp;</th>
-            <th>SUBTOTAL</th>
-        </tr>
-        <tr style="height: 6px; border-bottom: dashed; border-bottom-width: 1px;"></tr>
-    </thead>
-</table>
-
-<table class="section2">
-    <tbody>
-        <tr style="height: 3px;"></tr>
-        <tr>
-            <td colspan="2">1 01VVAM40</td>
-            <td>VAR ROS NC</td>
-            <td style="width: 80px;"></td>
-            <td>1/2-13</td>
-        </tr>
-    </tbody>
-</table>
-<table class="section2">
-    <tbody>
-        <tr>
-            <td style="width: 25%">PZ</td>
-            <td style="width: 25%">2.00</td>
-            <td style="width: 25%">16.46</td>
-            <td style="width: 25%">0.00</td>
-            <td style="width: 25%">32.92</td>
-        </tr>
-        <tr style="height: 6px; border-bottom: dashed; border-bottom-width: 1px;"></tr>
-    </tbody>
-</table>
+    <table class="section2">
+        <tbody>
+            <tr style="height: 3px;"></tr>
+            <tr style="font-size: 13px;">
+                <td colspan="2">1 01VVAM40</td>
+                <td>VAR ROS NC</td>
+                <td ></td>
+                <td>1/2-13</td>
+            </tr>
+            <tr style="font-size: 13px;">
+                <td style="width: 25%">PZ</td>
+                <td style="width: 25%">2.00</td>
+                <td style="width: 25%">16.46</td>
+                <td style="width: 25%">0.00</td>
+                <td style="width: 25%">32.92</td>
+            </tr>
+        </tbody>
+    </table>
+    <hr>
 
 
 
@@ -145,88 +142,90 @@
 
 
 
-<table class="section3">
-    <thead>
-        <tr style="height: 3px;"></tr>
-        <tr>
-            <th>SubTotal(BS) :</th>
-            <th style="text-align: right;padding-right: 18px;">32.92</th>
-        </tr>
-        <tr>
-            <th>Descuentos(BS) :</th>
-            <th style="text-align: right;padding-right: 18px;">0.00</th>
-        </tr>
-        <tr>
-            <th>Total(BS) :</th>
-            <th style="text-align: right;padding-right: 18px;">32.92</th>
-        </tr>
-        <tr>
-            <th colspan="2">Importe Base Credito Fiscal (BS) :</th>
-        </tr>
-        <tr>
-            <th style="border-bottom: solid 1px;"></th>
-            <th style="text-align: right;padding-right: 18px;">32.92</th>
-        </tr>
-        <tr style="height: 30px; border-bottom: dashed; border-bottom-width: 1px;">
-            <th colspan="2">Son: TREINTA Y DOS 92/100 BOLIVIANOS</th>
-        </tr>
-        <tr style="height: 5px;"></tr>
-    </thead>
-</table>
+    <table class="section3">
+        <thead>
+            <tr style="height: 3px;"></tr>
+            <tr>
+                <th>SubTotal(BS) :</th>
+                <th style="text-align: right;padding-right: 18px;">32.92</th>
+            </tr>
+            <tr>
+                <th>Descuentos(BS) :</th>
+                <th style="text-align: right;padding-right: 18px;">0.00</th>
+            </tr>
+            <tr>
+                <th>Total(BS) :</th>
+                <th style="text-align: right;padding-right: 18px;">32.92</th>
+            </tr>
+            <tr>
+                <th colspan="2">Importe Base Credito Fiscal (BS) :</th>
+            </tr>
+            <tr>
+                <th style="border-bottom: solid 1px;"></th>
+                <th style="text-align: right;padding-right: 18px;">32.92</th>
+            </tr>
+            <tr style="height: 25px;">
+                <th colspan="2">Son: TREINTA Y DOS 92/100 BOLIVIANOS</th>
+            </tr>
+        </thead>
+    </table>
+
+<hr>
 
 
+    <table class="section4">
+        <thead>
+            <tr>
+                <th>
+                    <p>ESTA FACTURA CONTRIBUYE AL DESARROLLO DEL PAÍS,</p>
+                    <P>EL USO ILÍCITO DE ESTA SERÁ SANCIONADO</P>
+                    <P>PENALMENTE DE ACUERDO A LEY.</P>
+                </th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr style="height: 5px;"></tr>
+            <tr>
+                <td>
+                    <P>Ley N° 453: El proveedor deberá entregar el producto en las</P>
+                    <p>modalidades y términos ofertados o convenidos.</p>
+                </td>
+            </tr>
+            <tr style="height: 5px;"></tr>
+            <tr>
+                <td>
+                    <p>Este documento es la representación gráfica de un Documento</p>
+                    <p>Fiscal Digital emitido en una modalidad de facturación en</p>
+                    <p>línea.</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <img src="https://static-unitag.com/images/help/QRCode/qrcode.png?mh=07b7c2a2" alt="" width="150px">
+                </td>
+            </tr>
+            <tr>
+                <td>FC341-57857 Vendedor: 34103 YUCRA ALEXANDER</td>
+            </tr>
+        </tbody>
+
+        <tfoot>
+            <tr style="font-size: 13px;">
+                <td>***PERNOS-TUERCAS-TORNILLOS***</td>
+            </tr>
+        </tfoot>
+    </table>
+</div>
 
 
-<table class="section4">
-    <thead>
-        <tr>
-            <th>
-                <p>ESTA FACTURA CONTRIBUYE AL DESARROLLO DEL PAÍS,</p>
-                <P>EL USO ILÍCITO DE ESTA SERÁ SANCIONADO</P>
-                <P>PENALMENTE DE ACUERDO A LEY.</P>
-            </th>
-        </tr>
-    </thead>
-
-    <tbody>
-        <tr style="height: 5px;"></tr>
-        <tr>
-            <td>
-                <P>Ley N° 453: El proveedor deberá entregar el producto en las</P>
-                <p>modalidades y términos ofertados o convenidos.</p>
-            </td>
-        </tr>
-        <tr style="height: 5px;"></tr>
-        <tr>
-            <td>
-                <p>Este documento es la representación gráfica de un Documento</p>
-                <p>Fiscal Digital emitido en una modalidad de facturación en</p>
-                <p>línea.</p>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <img src="https://static-unitag.com/images/help/QRCode/qrcode.png?mh=07b7c2a2" alt="" width="150px">
-            </td>
-        </tr>
-        <tr>
-            <td>FC341-57857 Vendedor: 34103 YUCRA ALEXANDER</td>
-        </tr>
-    </tbody>
-
-    <tfoot>
-        <tr>
-            <td>***PERNOS-TUERCAS-TORNILLOS***</td>
-        </tr>
-    </tfoot>
-</table>
 
 
 
 <style>
     table.minimalistBlack {
         font-family: Tahoma, Geneva, sans-serif;
-        width: 345px;
+        width: 8cm;
         text-align: center;
         border-collapse: collapse;
     }
@@ -257,7 +256,7 @@
 
     table.section1 {
         font-family: Tahoma, Geneva, sans-serif;
-        width: 345px;
+        width: 8cm;
         text-align: left;
         border-collapse: collapse;
     }
@@ -268,7 +267,7 @@
     }
 
     table.section1 tbody td {
-        font-size: 13px;
+        font-size: 11px;
     }
 
     table.section1 tfoot td {
@@ -277,7 +276,7 @@
 
     table.section2 {
         font-family: Tahoma, Geneva, sans-serif;
-        width: 345px;
+        width: 8cm;
         text-align: left;
         border-collapse: collapse;
     }
@@ -290,7 +289,7 @@
     table.section2 thead {}
 
     table.section2 thead th {
-        font-size: 15px;
+        font-size: 12px;
         font-weight: bold;
         letter-spacing: -0.08em;
     }
@@ -299,7 +298,7 @@
 
     table.section3 {
         font-family: Tahoma, Geneva, sans-serif;
-        width: 345px;
+        width: 8cm;
         text-align: left;
         border-collapse: collapse;
     }
@@ -312,7 +311,7 @@
     table.section3 thead {}
 
     table.section3 thead th {
-        font-size: 15px;
+        font-size: 12px;
         font-weight: bold;
         letter-spacing: -0.08em;
     }
@@ -322,7 +321,7 @@
 
 
     table.section4 {
-        width: 345px;
+        width: 8cm;
         text-align: center;
     }
 
@@ -340,7 +339,7 @@
     }
 
     table.section4 tbody td {
-        font-size: 0.74rem;
+        font-size: 0.73rem;
     }
 
     table.section4 tfoot {
@@ -350,4 +349,22 @@
     table.section4 p {
         margin: 0 0 0 0;
     }
+
+
+    hr {
+        color: #000;
+        height: 0px;
+        border:1px dashed;
+    }
 </style>
+
+
+
+
+
+
+
+
+{{--
+pixeles
+anchura: 302 --}}
